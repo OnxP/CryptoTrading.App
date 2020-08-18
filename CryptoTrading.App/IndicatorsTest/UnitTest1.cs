e12f3730c717d34c6c3fd7ef6b147f61b451c30b
OnxP <40940846+OnxP@users.noreply.github.com>
2020-08-18 23:29:36 +0100

    initial ceck in

---
+++ CryptoTrading.App/IndicatorsTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Indicators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IndicatorsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var results = Indicators.Indicators.ema;
+
+            Assert.IsNotNull(results);
+        }
+    }
+}
