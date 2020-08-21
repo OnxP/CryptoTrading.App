896eefbd26af75c7686de8e851d115efb6fd27d4
OnxP <ankurpatel0000@hotmail.com>
2020-08-22 00:34:19 +0100

    maket data load testing.

--- Src/CryptoTrading.App/CryptoTrading.App.MarketDataTesting/Program.cs
+++ Src/CryptoTrading.App/CryptoTrading.App.MarketDataTesting/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Binance;
 using Binance.Application;
@@ -20,9 +21,74 @@ namespace CryptoTrading.App.MarketDataTesting
         {
             var api = new BinanceApi();
 
-            // Check connectivity.
-            System.Threading.Tasks.Task task = PingAsync(api);
-            task.Wait();
+            //// Check connectivity.
+            //System.Threading.Tasks.Task task = PingAsync(api);
+            //task.Wait();
+
+            //LiveStream.StreamData();
+            var from = new DateTime(2020, 01, 29);
+            var to = new DateTime(2020, 07, 29);
+            var interval = CandlestickInterval.Hour;
+
+            foreach (DateTime dt in SplitDates(interval, from, to))
+            {
+                var task = LoadHistoricData(api, "BNBBTC", from, dt);
+                from = dt;
+                task.Wait();
+            }
+
+        }
+
+        private static async System.Threading.Tasks.Task LoadHistoricData(BinanceApi api, string symbol, DateTime from, DateTime to)
+        {
+            var test = await api.GetCandlesticksAsync(symbol, CandlestickInterval.Hour, 500, from.ToUniversalTime(), to.ToUniversalTime());
+
+            foreach (var item in test)
+            {
+                Console.WriteLine(item.OpenTime.ToString() + " - " + item.CloseTime.ToString());
+            }
+        }
+
+        protected static IEnumerable<DateTime> SplitDates(CandlestickInterval interval, DateTime from, DateTime to)
+        {
+            switch (interval)
+            {
+                case CandlestickInterval.Minutes_3:
+                case CandlestickInterval.Minutes_5:
+                case CandlestickInterval.Minutes_15:
+                case CandlestickInterval.Minutes_30:
+                    return SplitByDay(from, to);
+                    break;
+                case CandlestickInterval.Hour:
+                case CandlestickInterval.Hours_2:
+                case CandlestickInterval.Hours_4:
+                case CandlestickInterval.Hours_6:
+                case CandlestickInterval.Hours_8:
+                case CandlestickInterval.Hours_12:
+                    return SplitByWeek(from, to);
+                    break;
+                default:
+                    return new List<DateTime> { to };
+                    break;
+            }
+        }
+
+        protected static IEnumerable<DateTime> SplitByWeek(DateTime from, DateTime to)
+        {
+            var daysdiff = (to - from).TotalDays;
+            for (int i = 0; i < daysdiff; i++)
+            {
+                yield return from.AddDays(i);
+            }
+        }
+
+        protected static IEnumerable<DateTime> SplitByDay(DateTime from, DateTime to)
+        {
+            var daysdiff = (to - from).TotalDays;
+            for (int i = 1; i <= daysdiff; i++)
+            {
+                yield return from.AddDays(i);
+            }
         }
 
         public static async System.Threading.Tasks.Task PingAsync(BinanceApi api)
@@ -32,161 +98,6 @@ namespace CryptoTrading.App.MarketDataTesting
                 Console.WriteLine("Successful!");
             }
         }
-        public static void StreamData()
-        {
-            try
-            {
-                // Load configuration.
-                var configuration = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json", true, false)
-                    .Build();
 
-                // Configure services.
-                var services = new ServiceCollection()
-                    .AddBinance() // add default Binance services.
-                    .AddLogging(builder => builder // configure logging.
-                        .SetMinimumLevel(LogLevel.Trace)
-                        .AddFile(configuration.GetSection("Logging:File")))
-                    .BuildServiceProvider();
-
-                // Get configuration settings.
-                var symbols = configuration.GetSection("CombinedStreamsExample:Symbols").Get<string[]>()
-                    ?? new string[] { Symbol.BTC_USDT };
-
-                var interval = CandlestickInterval.Minute;
-                try { interval = configuration.GetSection("PriceChart")?["Interval"].ToCandlestickInterval() ?? CandlestickInterval.Minute; }
-                catch { /* ignore */ }
-
-                // Initialize client.
-                var client = services.GetService<ICandlestickClient>();
-
-                // Initialize the stream.
-                var webSocket = services.GetService<IBinanceWebSocketStream>();
-
-                // Initialize controller.
-                using (var controller = new RetryTaskController(webSocket.StreamAsync))
-                {
-                    controller.Error += (s, e) => HandleError(e.Exception);
-
-                    if (symbols.Length == 1)
-                    {
-                        // Subscribe to symbol with callback.
-                        client.Subscribe(symbols[0], interval, Display);
-                    }
-                    else
-                    {
-                        // Alternative usage (combined streams).
-                        client.Candlestick += (s, evt) => { Display(evt); };
-
-                        // Subscribe to each of the symbols.
-                        foreach (var symbol in symbols)
-                        {
-                            client.Subscribe(symbol, interval); // using event instead of callbacks.
-                        }
-                    }
-
-                    // Set stream URI using client subscribed streams.
-                    webSocket.Uri = BinanceWebSocketStream.CreateUri(client);
-                    // NOTE: This must be done after client subscribe.
-
-                    // Route stream messages to client.
-                    webSocket.Message += (s, e) => client.HandleMessage(e.Subject, e.Json);
-
-                    // Begin streaming.
-                    controller.Begin();
-
-                    _message = "...press any key to continue.";
-                    Console.ReadKey(true); // wait for user input.
-
-                    //*//////////////////////////////////////////////////
-                    // Example: Unsubscribe/Subscribe after streaming...
-                    /////////////////////////////////////////////////////
-
-                    // NOTE: When the URI is changed, the web socket is aborted
-                    //       and a new connection is made. There's a delay
-                    //       before streaming begins to allow for multiple
-                    //       changes.
-
-                    // Unsubscribe a symbol.
-                    client.Unsubscribe(symbols[0], interval);
-
-                    // Subscribe to XRP.
-                    client.Subscribe(Symbol.XRP_USDT, interval); // drink the kool-aid.
-
-                    // Set stream URI using client subscribed streams.
-                    webSocket.Uri = BinanceWebSocketStream.CreateUri(client);
-                    // NOTE: This must be done after client subscribe.
-
-                    lock (_sync)
-                    {
-                        // Remove unsubscribed symbol and clear display (application specific).
-                        _candlesticks.Remove(symbols[0]);
-                        Console.Clear();
-                    }
-
-                    _message = "...press any key to exit.";
-                    Console.ReadKey(true); // wait for user input.
-                                           ///////////////////////////////////////////////////*/
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine();
-                Console.WriteLine("  ...press any key to close window.");
-                Console.ReadKey(true);
-            }
-        }
-        private static string _message;
-
-        // ReSharper disable once InconsistentNaming
-        private static readonly object _sync = new object();
-
-        // ReSharper disable once InconsistentNaming
-        private static readonly IDictionary<string, Candlestick> _candlesticks
-            = new SortedDictionary<string, Candlestick>();
-
-        private static Task _displayTask = Task.CompletedTask;
-
-        private static void Display(CandlestickEventArgs args)
-        {
-            lock (_sync)
-            {
-                _candlesticks[args.Candlestick.Symbol] = args.Candlestick;
-
-                if (_displayTask.IsCompleted)
-                {
-                    // Delay to allow multiple data updates between display updates.
-                    _displayTask = Task.Delay(250)
-                        .ContinueWith(_ =>
-                        {
-                            Candlestick[] latestCandlesticks;
-                            lock (_sync)
-                            {
-                                latestCandlesticks = _candlesticks.Values.ToArray();
-                            }
-
-                            Console.SetCursorPosition(0, 0);
-
-                            foreach (var c in latestCandlesticks)
-                            {
-                                Console.WriteLine($" {c.Symbol} - O: {c.Open:0.00000000} | H: {c.High:0.00000000} | L: {c.Low:0.00000000} | C: {c.Close:0.00000000} | V: {c.Volume:0.00} - [{c.OpenTime.ToLongTimeString()}]".PadRight(119));
-                                Console.WriteLine();
-                            }
-
-                            Console.WriteLine(_message.PadRight(119));
-                        });
-                }
-            }
-        }
-
-        private static void HandleError(Exception e)
-        {
-            lock (_sync)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
     }
 }
