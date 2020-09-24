fee793279cf88d66769ef8363b108536d3083f31
OnxP <ankurpatel0000@hotmail.com>
2020-09-24 23:19:22 +0100

    stoploss monitor,.

---
+++ Src/CryptoTrading.App/CryptoTrading.App.Core/IBroker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoTrading.App.Core
+{
+    public interface IBroker
+    {
+        void SetLimitOrder(ITrade trade, decimal currentStopLoss);
+        void SetNewLimitOrder(ITrade trade, decimal currentStopLoss);
+        void ClosePosition(ITrade trade);
+    }
+}
---
+++ Src/CryptoTrading.App/CryptoTrading.App.Core/ITrade.cs
@@ -0,0 +1,7 @@
+﻿namespace CryptoTrading.App.Core
+{
+    public interface ITrade
+    {
+        decimal Price { get; set; }
+    }
+}
