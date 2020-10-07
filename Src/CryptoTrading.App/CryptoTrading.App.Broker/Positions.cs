6bee49a6266320c1fd3f2d8b0c12efa453fc4ca9
OnxP <40940846+OnxP@users.noreply.github.com>
2020-10-07 23:36:00 +0100

    added osittons class

---
+++ Src/CryptoTrading.App/CryptoTrading.App.Broker/Positions.cs
@@ -0,0 +1,32 @@
+﻿using Binance;
+using CryptoTrading.App.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoTrading.App.Broker
+{
+    public class Positions : IPositions
+    {
+        public Dictionary<string, IPosition> _positions;
+        public bool CheckBalance(string sellSymbol, string sellAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckOpenPosition(string requestBuySymbol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrade CreateTrade(ITradeRequest request, StopLossMonitor stopLossMonitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePosition(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
